d81bfaa24892eb43dbc75b3edf41b3ee34744e9e
Coding Wolf <codingwolf12@gmail.com>
2026-05-13 14:39:42 +0530

    Update pattern.cpp

--- pattern.cpp
+++ pattern.cpp
@@ -310,6 +310,50 @@ void patternM(int n)
 }
 
 
+void patternN(int n)
+// *******
+//  *****
+//   ***
+//    *
+//   ***
+//  *****
+// *******
+{
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < i; j++)
+        {
+            cout << " ";
+        }
+        for (int j = 0; j < n - i; j++)
+        {
+            cout << "*";
+        }
+        for (int j = 0; j < n - i - 1; j++)
+        {
+            cout << "*";
+        }
+        cout << endl;
+    }
+    for (int i = 0; i < n - 1; i++)
+    {
+        for (int j = 0; j < n - 2 - i; j++)
+        {
+            cout << " ";
+        }
+        for (int j = 0; j < i + 2; j++)
+        {
+            cout << "*";
+        }
+        for (int j = 0; j < i + 1; j++)
+        {
+            cout << "*";
+        }
+        cout << endl;
+    }
+}
+
+
 int main()
 {
     // patternA(4);
