3cae468bba64dc17c186e0a5c1223f5820b3ae73
Coding Wolf <codingwolf12@gmail.com>
2026-05-13 23:42:11 +0530

    Update pattern.cpp

--- pattern.cpp
+++ pattern.cpp
@@ -475,7 +475,50 @@ void patternS(int n)
 }
 
 
-
+void patternT(int n)
+//    *
+//   * *
+//  *   *
+// *     *
+//  *   *
+//   * *
+//    *
+{
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n - i - 1; j++)
+        {
+            cout << " ";
+        }
+        cout << "*";
+        if (i != 0)
+        {
+            for (int j = 0; j < 2 * i - 1; j++)
+            {
+                cout << " ";
+            }
+            cout << "*";
+        }
+        cout << endl;
+    }
+    for (int i = 0; i < n - 1; i++)
+    {
+        for (int j = 0; j < i + 1; j++)
+        {
+            cout << " ";
+        }
+        cout << "*";
+        if (i != n - 2)
+        {
+            for (int j = 0; j < 2 * (n - i) - 5; j++)
+            {
+                cout << " ";
+            }
+            cout << "*";
+        }
+        cout << endl;
+    }
+}
 int main()
 {
     // patternA(4);
@@ -497,5 +540,6 @@ int main()
     // patternQ(4);
     // patternR(4);
     // patternS(4);
+    // patternT(4);
     return 0;
 }
